7d3da99f4cf31fb9841362bf03ca4b79fb8b0551
Daniele Paz <danielepaz404@gmail.com>
2025-06-23 17:33:13 -0300

    :sparkles: Add forms

--- src/app/core/create/create.component.ts
+++ src/app/core/create/create.component.ts
@@ -1,10 +1,22 @@
 import { Component } from "@angular/core";
+import { Form, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 
 @Component({
     selector: '',
     templateUrl: './create.component.html',
-    styleUrl: './create.component.scss'
+    styleUrl: './create.component.scss',
+    imports: [ReactiveFormsModule, FormsModule]
 })
 export class CreateComponent {
-    
+    reactiveForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder){
+        this.reactiveForm = this.formBuilder.group({
+            description: ['',  [ Validators.required, Validators.minLength(4)]],
+            priority: ['']
+        });
+    }
+
+    descriptionTemplateControl = '';
+    priorityTemplateControl = '';
 }
